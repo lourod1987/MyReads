0c123c21d65602ad918bcdf5d58481373a81468b
lourod1987 <louart87@gmail.com>
2018-09-27 18:30:25 -0500

    commening out a img import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { MyBooksApp } from './MyBooksApp.js';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
