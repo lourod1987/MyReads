054dca398a223c317177a45854d9c571bd6c74ad
lourod1987 <louart87@gmail.com>
2018-09-27 19:16:30 -0500

    importing Links from react-router-dom and PropTypes from prop-types. Building out basic structure of MyBooksPage

--- src/MyBooksPage.js
+++ src/MyBooksPage.js
@@ -1,26 +1,89 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import PropTypes from 'prop-types';
 
 export class MyBooksPage extends Component {
+    // static propTypes = {
+        
+    // }
 
     render() {
+        const { books } = this.props;
+
+        const reading = books.filter( book => (
+            book.shelf === 'currentlyReading'
+        ));
+
+        const wantToRead = books.filter( book => (
+            book.shelf === 'wantToRead'
+        ));
+
+        const doneReading = books.filter( book => (
+            book.shelf === 'read'
+        ));
+
         return (
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">My Reads Library</h1>
+                    <Link
+                        to="/search"
+                    >Search</Link>
                 </header>
                 <main>
+                        <h2>Currently Reading</h2>
                     <ul>
-                        <li>{}</li>
+                        {reading.map( book => (
+                            <li key={book.id}>
+                            <figure>
+                                <img src={book.imageLinks.thumbnail} alt={`Book cover for ${book.title}`}/>
+                                <div className="dropdown">
+                                    <button className="dropbtn">+</button>
+                                    <div className="dropdown-content">
+                                        <a>Currently Reading</a>
+                                        <a >Want to Read</a>
+                                        <a >Done Reading</a>
+                                        <a >None</a>
+                                    </div>
+                                </div>
+                                <figcaption>
+                                    <p>Title: {book.title}</p>
+                                    <p>Author: {book.authors}</p>
+                                </figcaption>
+                            </figure>
+                            </li>
+                        ))}
                     </ul>
+                    <h2>Want to Read</h2>
                     <ul>
-                        <li>{}</li>
+                        {wantToRead.map( book => (
+                            <li key={book.id}>
+                            <figure>
+                                <img src={book.imageLinks.thumbnail} alt={`Book cover for ${book.title}`}/>
+                                <button></button>
+                                <figcaption>
+                                    <p>Title: {book.title}</p>
+                                    <p>Author: {book.authors}</p>
+                                </figcaption>
+                            </figure>
+                            </li>
+                        ))}
                     </ul>
+                    <h2>Done Reading</h2>
                     <ul>
-                        <li>{}</li>
+                        {doneReading.map( book => (
+                            <li key={book.id}>
+                            <figure>
+                                <img src={book.imageLinks.thumbnail} alt={`Book cover for ${book.title}`}/>
+                                <button></button>
+                                <figcaption>
+                                    <p>Title: {book.title}</p>
+                                    <p>Author: {book.authors}</p>
+                                </figcaption>
+                            </figure>
+                            </li>
+                        ))}
                     </ul>
-                    {/* <currentReads></currentReads>
-                    <wantToRead></wantToRead>
-                    <alreadyRead></alreadyRead> */}
                 </main>
             </div>
         );
