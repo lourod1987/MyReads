7be94e7a02047ca2c365882f8993553188b0d0c1
lourod1987 <louart87@gmail.com>
2018-10-02 02:13:45 -0500

    adding in proptypes to ensure correct props are passed, shelf add and handle click methods, deconstructing props keys, correcting formatting for books app rendering

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 export class SearchPage extends Component {
+    //add a function that checks if getAll books.id matches searchResults book.id, if yes add shelf property to searchResults book with correct value
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        searchResults: PropTypes.array.isRequired,
+        query: PropTypes.string.isRequired,
+        onChange: PropTypes.func.isRequired
+    }
+
+    shelfAdd = (searchResults, books) => {
+        for (let i = 0; i < books.length; i++) {
+            for (let j = 0; j < searchResults.length; j++) {
+                if (books[i].id === searchResults[j].id) {
+                    searchResults[j].shelf = books[i].shelf;
+                    console.log(searchResults[j].shelf);
+                    console.log(searchResults);
+                }
+            }
+        }
+        return searchResults;
+    }
+
+    handleClick = (id, newShelf) => {
+        let bookIndex;
+        for (let i = 0; i < this.props.searchResults.length; i++) {
+            if (this.props.searchResults[i].id === id) {
+                bookIndex = this.props.searchResults[i];
+            }
+        }
+        
+        console.log(bookIndex);
+        this.props.handleShelf(bookIndex, newShelf);
+    }
 
     render() {
-        const { searchResults, query, onChange } = this.props
+        const { searchResults, books, query, onChange } = this.props
         
-        // const searchTerms = books.filter( book => (
-        //     book.title == query
-        // ));
         return (
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">My Reads Library</h1>
                     <Link
-                            to="/"
+                        to="/"
                         >My Books</Link>
                 </header>
                 <main>
@@ -23,34 +52,53 @@ export class SearchPage extends Component {
                         <input 
                             type="text" 
                             placeholder='Search'
-                            value={query} 
+                            value={query}
                             onChange={onChange}
                         />
                     </form>
                     {JSON.stringify(query)}
                     <h2>Search Results</h2>
-                    {(searchResults === undefined) ? 
-                    <h3>No search results found</h3> : (
+                    {(searchResults.length === 0) ? 
+                    <h3>No matches found for your search</h3> : (
                         <ul>
-                            {searchResults.map( book => (
+                            {this.shelfAdd(searchResults, books).map( book => (
                                 <li key={book.id}>
-                                <figure>
-                                    {book.imageLinks !== undefined && (
-                                    <img src={book.imageLinks.thumbnail} alt={`Book cover for ${book.title}`}/>)}
-                                    <div className="dropdown">
-                                        <button className="dropbtn">+</button>
-                                        <div className="dropdown-content">
-                                            <a >Currently Reading</a>
-                                            <a >Want to Read</a>
-                                            <a >Done Reading</a>
-                                            <a >None</a>
+                                    <figure>
+                                        {book.imageLinks !== undefined && (
+                                            <img 
+                                                src={book.imageLinks.thumbnail} 
+                                                alt={`Book cover for ${book.title}`}
+                                                className="book-cover"
+                                            />
+                                        )}
+                                        <div className="dropdown">
+                                            <button className="dropbtn">+</button>
+                                            <div className="dropdown-content">
+                                                <a
+                                                    onClick={() => this.handleClick(book.id, 'currentlyReading')}
+                                                    className={(book.shelf === 'currentlyReading') ? "highlight" : ""}>Currently Reading</a>
+                                                <a
+                                                    onClick={() => this.handleClick(book.id, 'wantToRead')}
+                                                    className={(book.shelf === 'wantToRead') ? "highlight" : ""}>Want to Read</a>
+                                                <a
+                                                    onClick={() => this.handleClick(book.id, 'read')} 
+                                                    className={(book.shelf === 'read') ? "highlight" : ""}>Done Reading</a>
+                                                <a
+                                                    onClick={() => this.handleClick(book.id, 'undefined')} 
+                                                    className={(book.shelf === undefined) ? "highlight" : ""}>None</a>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <figcaption>
-                                        <p>Title: {book.title}</p>
-                                        <p>Author: {book.authors}</p>
-                                    </figcaption>
-                                </figure>
+                                        <figcaption className="book-cover-title">
+                                            <p>Title: {book.title}</p>
+                                            {book.authors !== undefined && (
+                                                <ul className="author">
+                                                    {book.authors.map( author => (
+                                                        <li key={author}>Author: {author}</li>
+                                                    ))}
+                                                </ul>
+                                            )}
+                                        </figcaption>
+                                    </figure>
                                 </li>
                             ))}
                         </ul>
