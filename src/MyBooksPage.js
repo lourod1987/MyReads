e1f9c8df224c9eaa81bd52f79b225ffaac93122c
lourod1987 <louart87@gmail.com>
2018-10-02 02:29:39 -0500

    adding in proptypes for ensuring necessary props are passed, handleClick and handFilter methods added, deconstructing only necssary props, eliminated repeated filters (method replaced), removed repeated book list code and created a separate component that is called in to render and passed all necessary props

--- src/MyBooksPage.js
+++ src/MyBooksPage.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { BookList } from './BookList';
 
 export class MyBooksPage extends Component {
-    // static propTypes = {
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        handleShelf: PropTypes.func.isRequired,
+    }
+
+    handleClick = (id, newShelf, books) => {
+        let bookIndex;
+        for (let i = 0; i < this.props.books.length; i++) {
+            if (this.props.books[i].id === id) {
+                bookIndex = this.props.books[i];
+            }
+        }
         
-    // }
+        // console.log(bookIndex);
+        this.props.handleShelf(bookIndex, newShelf, books);
+        // this.handleFilter(this.props.books, newShelf)
+    }
+
+    handleFilter = (books, filter) => {
+        return books.filter( book => (
+            book.shelf === filter
+        ));
+    }
 
     render() {
-        const { books } = this.props;
-
-        const reading = books.filter( book => (
-            book.shelf === 'currentlyReading'
-        ));
-
-        const wantToRead = books.filter( book => (
-            book.shelf === 'wantToRead'
-        ));
-
-        const doneReading = books.filter( book => (
-            book.shelf === 'read'
-        ));
+        const { books, handleShelf } = this.props;
 
         return (
             <div className="App">
@@ -31,59 +40,31 @@ export class MyBooksPage extends Component {
                     >Search</Link>
                 </header>
                 <main>
-                        <h2>Currently Reading</h2>
-                    <ul>
-                        {reading.map( book => (
-                            <li key={book.id}>
-                            <figure>
-                                <img src={book.imageLinks.thumbnail} alt={`Book cover for ${book.title}`}/>
-                                <div className="dropdown">
-                                    <button className="dropbtn">+</button>
-                                    <div className="dropdown-content">
-                                        <a>Currently Reading</a>
-                                        <a >Want to Read</a>
-                                        <a >Done Reading</a>
-                                        <a >None</a>
-                                    </div>
-                                </div>
-                                <figcaption>
-                                    <p>Title: {book.title}</p>
-                                    <p>Author: {book.authors}</p>
-                                </figcaption>
-                            </figure>
-                            </li>
-                        ))}
-                    </ul>
+                    <h2>Currently Reading</h2>
+                    <BookList
+                        books={books}
+                        handleShelf={handleShelf}
+                        handleClick={this.handleClick}
+                        handleFilter={this.handleFilter}
+                        shelfFilter="currentlyReading"
+                    />
                     <h2>Want to Read</h2>
-                    <ul>
-                        {wantToRead.map( book => (
-                            <li key={book.id}>
-                            <figure>
-                                <img src={book.imageLinks.thumbnail} alt={`Book cover for ${book.title}`}/>
-                                <button></button>
-                                <figcaption>
-                                    <p>Title: {book.title}</p>
-                                    <p>Author: {book.authors}</p>
-                                </figcaption>
-                            </figure>
-                            </li>
-                        ))}
-                    </ul>
+                    <BookList
+                        books={books}
+                        handleShelf={this.handleShelf}
+                        handleClick={this.handleClick}
+                        handleFilter={this.handleFilter}
+                        shelfFilter="wantToRead"
+                    />
+                    
                     <h2>Done Reading</h2>
-                    <ul>
-                        {doneReading.map( book => (
-                            <li key={book.id}>
-                            <figure>
-                                <img src={book.imageLinks.thumbnail} alt={`Book cover for ${book.title}`}/>
-                                <button></button>
-                                <figcaption>
-                                    <p>Title: {book.title}</p>
-                                    <p>Author: {book.authors}</p>
-                                </figcaption>
-                            </figure>
-                            </li>
-                        ))}
-                    </ul>
+                    <BookList
+                            books={books}
+                            handleShelf={this.handleShelf}
+                            handleClick={this.handleClick}
+                            handleFilter={this.handleFilter}
+                            shelfFilter="read"
+                    />
                 </main>
             </div>
         );
