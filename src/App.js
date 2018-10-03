085da6ab149aed737c72d5354c63015b2d01372b
lourod1987 <louart87@gmail.com>
2018-10-03 15:32:00 -0500

    rewriting App as a stateless functional component since no state is used in component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { MyBooksApp } from './MyBooksApp.js';
 // import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+function App (props) {
     return (
       <MyBooksApp />
     );
-  }
 }
 
 export default App;
