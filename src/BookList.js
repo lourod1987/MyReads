2c614399b9f0123b1942aa500fdfe509cd94b1d1
lourod1987 <louart87@gmail.com>
2018-10-03 01:36:09 -0500

    remove unecessary props. Add display if expression, if no results for shelf display h3 None.

--- src/BookList.js
+++ src/BookList.js
@@ -4,59 +4,63 @@ import PropTypes from 'prop-types';
 export class BookList extends Component {
 
     static propTypes = {
-        allMyBooks: PropTypes.array.isRequired,
         handleClick: PropTypes.func.isRequired,
         handleFilter: PropTypes.func.isRequired,
         shelfFilter: PropTypes.string.isRequired,
     }
 
     render() {
-        const { allMyBooks, handleClick, handleFilter, shelfFilter } = this.props;
-
+        const { handleClick, handleFilter, shelfFilter } = this.props;
+        
         return (
-            <ul>
-            {handleFilter(allMyBooks, shelfFilter).map( (book) => (
-                <li key={book.id}>
-                    <figure>
-                        {book.imageLinks !== undefined && (
-                            <img
-                                src={book.imageLinks.thumbnail} 
-                                alt={`Book cover for ${book.title}`}
-                                className="book-cover"
-                            />
-                        )}
-                        <div className="dropdown">
-                            <button className="dropbtn">+</button>
-                            <div className="dropdown-content">
-                                <a
-                                    onClick={() => handleClick(book.id, 'currentlyReading')}
-                                    className={(book.shelf === 'currentlyReading') ? "highlight" : ""}>Currently Reading</a>
-                                <a
-                                    onClick={() => handleClick(book.id, 'wantToRead')}
-                                    className={(book.shelf === 'wantToRead') ? "highlight" : ""}
-                                >Want to Read</a>
-                                <a
-                                    onClick={() => handleClick(book.id, 'read')}
-                                    className={(book.shelf === 'read') ? "highlight" : ""}>Done Reading</a>
-                                <a 
-                                    onClick={() => handleClick(book.id, 'undefined')}
-                                    className={(book.shelf === undefined || book.shelf === '') ? "highlight" : ""}>None</a>
-                            </div>
-                        </div>
-                        <figcaption className="book-cover-title">
-                            <p>Title: {book.title}</p>
-                            {book.authors !== undefined && (
-                                <ul className="author">
-                                    {book.authors.map( author => (
-                                        <li key={author}>Author: {author}</li>
-                                    ))}
-                                </ul>
+            <div>
+            {(handleFilter(shelfFilter) === undefined) ? 
+                <h3>None</h3> : (
+                <ul>
+                {handleFilter(shelfFilter).map( (book) => (
+                    <li key={book.id}>
+                        <figure>
+                            {book.imageLinks !== undefined && (
+                                <img
+                                    src={book.imageLinks.thumbnail} 
+                                    alt={`Book cover for ${book.title}`}
+                                    className="book-cover"
+                                />
                             )}
-                        </figcaption>
-                    </figure>
-                </li>
-            ))}
-        </ul>
+                            <div className="dropdown">
+                                <button className="dropbtn">+</button>
+                                <div className="dropdown-content">
+                                    <a
+                                        onClick={() => handleClick(book.id, 'currentlyReading')}
+                                        className={(book.shelf === 'currentlyReading') ? "highlight" : ""}>Currently Reading</a>
+                                    <a
+                                        onClick={() => handleClick(book.id, 'wantToRead')}
+                                        className={(book.shelf === 'wantToRead') ? "highlight" : ""}
+                                    >Want to Read</a>
+                                    <a
+                                        onClick={() => handleClick(book.id, 'read')}
+                                        className={(book.shelf === 'read') ? "highlight" : ""}>Done Reading</a>
+                                    <a 
+                                        onClick={() => handleClick(book.id, 'undefined')}
+                                        className={(book.shelf === undefined || book.shelf === '') ? "highlight" : ""}>None</a>
+                                </div>
+                            </div>
+                            <figcaption className="book-cover-title">
+                                <p>Title: {book.title}</p>
+                                {book.authors !== undefined && (
+                                    <ul className="author">
+                                        {book.authors.map( author => (
+                                            <li key={author}>Author: {author}</li>
+                                        ))}
+                                    </ul>
+                                )}
+                            </figcaption>
+                        </figure>
+                    </li>
+                ))}
+            </ul>
+            )}
+        </div>
         )
     }
 }
